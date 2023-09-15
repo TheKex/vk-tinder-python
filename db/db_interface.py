d95fb22efb13132b87aaa3ee2b31d207e74e8b16
Алексей Томилин <al.tomilin@bars-open.group>
2023-09-15 10:51:25 +0300

    db + bot

---
+++ db/db_interface.py
@@ -0,0 +1,65 @@
+from os import getenv
+from enum import Enum
+
+from dotenv import load_dotenv
+import sqlalchemy as db
+from sqlalchemy.orm import sessionmaker
+
+from models import create_tables, User, Favorite, Phote, BlackList
+
+
+def add_user(db_session: db.orm.Session,
+             vk_user_id: int,
+             first_name: str,
+             last_name: str,
+             sex: int,
+             age: int,
+             link: str,
+             photos: list):
+    user = User()
+    user.vk_user_id = vk_user_id
+    user.first_name = first_name
+    user.last_name = last_name
+    user.sex = sex
+    user.age = age
+    user.link = link
+
+    db_session.add(user)
+
+    for photo_data in photos:
+        photo = Phote()
+        photo.user = user
+        photo.photo_data = photo_data
+        db_session.add(photo)
+
+    db_session.commit()
+
+
+def get_user_by_vk_id(db_session: db.orm.Session, user_vk_id: int):
+    user = db_session.query(User).where(User.vk_user_id == user_vk_id).first()
+    return user
+
+
+def add_to_favorite(db_session: db.orm.Session, user: User, favorite: User):
+    favorite_user = Favorite()
+    favorite_user.user = user
+    favorite_user.favorite_user = favorite
+    db_session.add(favorite_user)
+    db_session.commit()
+
+
+
+load_dotenv()
+db_host = getenv('DB_HOST')
+db_port = getenv('DB_PORT')
+db_user = getenv('DB_USER')
+db_pass = getenv('DB_PASS')
+db_name = getenv('DB_NAME')
+
+print(f"postgresql+psycopg2://{db_user}:{db_pass}@{db_host}/{db_name}")
+
+engine = db.create_engine(f"postgresql+psycopg2://{db_user}:{db_pass}@{db_host}/{db_name}",
+                          echo=True)
+
+Session = sessionmaker(bind=engine)
+session = Session()
---
+++ db/models.py
@@ -0,0 +1,78 @@
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+from os import getenv
+
+from dotenv import load_dotenv
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    vk_user_id = sq.Column(sq.Integer, unique=True)
+    first_name = sq.Column(sq.String(length=40))
+    last_name = sq.Column(sq.String(length=40))
+    sex = sq.Column(sq.Integer)
+    age = sq.Column(sq.Integer)
+    link = sq.String(length=400)
+
+    def __str__(self):
+        return f"User: {self.vk_user_id}: {self.first_name} {self.last_name}"
+
+
+class Favorite(Base):
+    __tablename__ = "favorite"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    user = sq.Column(sq.Integer, sq.ForeignKey("user.id"))
+    favorite_user = sq.Column(sq.Integer, sq.ForeignKey("user.id"))
+
+    def __str__(self):
+        return (f"User: {self.user.first_name} {self.user.last_name}; "
+                f"Favorite:{self.favorite_user.first_name} {self.favorite_user.last_name}")
+
+
+class BlackList(Base):
+    __tablename__ = "blacklist"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    user = sq.Column(sq.Integer, sq.ForeignKey("user.id"))
+    black_listed_user = sq.Column(sq.Integer, sq.ForeignKey("user.id"))
+
+    def __str__(self):
+        return (f"User: {self.user.first_name} {self.user.last_name}; "
+                f"Favorite:{self.black_listed_user.first_name} {self.black_listed_user.last_name}")
+
+
+class Phote(Base):
+    __tablename__ = "photo"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    user = sq.Column(sq.Integer, sq.ForeignKey("user.id"))
+    photo_data = sq.Column(sq.LargeBinary)
+
+
+def create_tables(engine):
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    db_host = getenv('DB_HOST')
+    db_port = getenv('DB_PORT')
+    db_user = getenv('DB_USER')
+    db_pass = getenv('DB_PASS')
+    db_name = getenv('DB_NAME')
+
+    print(f"postgresql+psycopg2://{db_user}:{db_pass}@{db_host}/{db_name}")
+
+    engine = sq.create_engine(f"postgresql+psycopg2://{db_user}:{db_pass}@{db_host}/{db_name}",
+                              echo=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    create_tables(engine)
